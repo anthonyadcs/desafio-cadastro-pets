65dd816b0ca68ef3fea78943d49ec9f10307fb75
Anthony <anthonydacsilva10@gmail.com>
2026-02-11 20:37:18 -0300

    feat: initialize application structure with Main and basic CLI menu

---
+++ src/ui/Menu.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class Menu {
+    public static void showOptions() {
+        System.out.println("[ 1 ] Cadastrar um novo pet");
+        System.out.println("[ 2 ] Alterar os dados do pet cadastrado");
+        System.out.println("[ 3 ] Deletar um pet cadastrado");
+        System.out.println("[ 4 ] Listar todos os pets cadastrados");
+        System.out.println("[ 5 ] Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("[ 6 ] Sair");
+    }
+}
